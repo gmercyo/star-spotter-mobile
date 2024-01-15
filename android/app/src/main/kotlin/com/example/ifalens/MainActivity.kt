a9054cef7b8580d03ecd70255293547897a3529c
Temitope Ilesanmi <topephysics@gmail.com>
2024-01-15 13:55:02 +0000

    setup configs for build

--- android/app/src/main/kotlin/com/example/ifalens/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.star-spotter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.star-spotter
+package com.star_spotter.app
 
 import io.flutter.embedding.android.FlutterActivity
 
